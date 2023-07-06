9135cb24f7c18b200430557cef7b3facf20d3e57
unknown <sg03056924122@gmail.com>
2023-07-06 11:39:10 +0500

    Orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -13,7 +14,8 @@ class OrderController extends Controller
      */
     public function index()
     {
-        return view('order.show');
+        $orders = Order::get();
+        return view('order.show', compact('orders'));
     }
 
     /**
